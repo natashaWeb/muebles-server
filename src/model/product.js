6db13a613f7a62ecfcff6443c1216c38d4bf1b17
natasha <natasha87050@gmail.com>
2024-06-14 15:22:10 -0300

    Productos predefinidos

--- src/model/product.js
+++ src/model/product.js
@@ -19,6 +19,39 @@ const productoSchema = new mongoose.Schema({
     },
 });
 
+async function predefinidos() {
+    try {
+        const count = await Producto.countDocuments();
+
+        if (count === 0) {
+            await Producto.create([
+                {
+                    titulo: "Table",
+                    autor: "The Rock",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen1.png",
+                },
+                {
+                    titulo: "Chair",
+                    autor: "Messi",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen2.png",
+                },
+                {
+                    titulo: "Couch",
+                    autor: "Suares",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen3.png",
+                },
+            ]);
+        }
+    } catch (error) {
+        console.error("Error al agregar datos predefinidos:", error);
+    }
+}
+
 const Producto = mongoose.model("Producto", productoSchema);
+
+predefinidos()
 
 module.exports = Producto;
