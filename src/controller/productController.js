5ed0439274612d607db8e978bc9e0f58dfe4a86f
natasha <natasha87050@gmail.com>
2024-06-14 15:50:46 -0300

    Cambios Base de datos

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,4 +1,37 @@
 const Product = require("../model/product");
+
+async function predefinidos() {
+    try {
+        const count = await Product.countDocuments();
+        if (count === 0) {
+            await Product.create([
+                {
+                    titulo: "Table",
+                    autor: "The Rock",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen1.png",
+                },
+                {
+                    titulo: "Chair",
+                    autor: "Messi",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen2.png",
+                },
+                {
+                    titulo: "Couch",
+                    autor: "Suares",
+                    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fermentum lorem, eget laoreet est.",
+                    imagen: "/imagen3.png",
+                },
+            ]);
+        }
+    } catch (error) {
+        console.error("Error al agregar datos predefinidos:", error);
+    }
+}
+
+predefinidos();
+
 const messageController = {
     async read(req, res) {
         try {
